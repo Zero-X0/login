ac884dda770f2872ddd4d82147e08020ac97db74
Zero-X0 <Zero-X0@users.noreply.github.com>
2020-03-18 09:27:37 -0600

    Update login.py

--- login.py
+++ login.py
@@ -21,29 +21,21 @@ L03.place(x=45, y=170)#Esto es la ubicacion del label
 L03.config( fg="blue")#color de fondo y de las letras en el label
 L03.config(fon=("arial black",16))
 
-#def imprimir():
- #   l01=Label(text="Usuario:"+t01.get()+" Password:" +t02.get())
-  #  l01.pack()
-   # l01.place(x=1,y=1)
-    #l01.config(bg="orange", fg="white")
-    #l01.config(fon=("arial", 10))
-
 #Campo de texto
-#t01= Entry(miFrame)
-#t01.pack()
-#t01.config(fon=("arial black", 16))
-#t01.config(bg="red",fg="blue")
-#t02= Entry(Frame)
-#t02.pack()
-#t02.config(fon=("arial black", 16))
-#t02.config(bg="red",fg="blue")
+t01= Entry(miFrame)
+t01.pack()
+t01.config(fon=("arial black", 16))
+t01.config(bg="white",fg="blue")
+t02= Entry(Frame, show="*")
+t02.pack()
+t02.config(fon=("arial black", 16))
+t02.config(bg="white",fg="blue")
 
-
-#b01=Button(text="Login", command=imprimir)
-#b01.pack()
-#b01.place(x=270,y=250)
-#b02=Button(text="Registro")
-#b02.pack()
-#b02.place(x=320,y=250)
+b01=Button(text="Login")
+b01.pack()
+b01.place(x=270,y=250)
+b02=Button(text="Registro")
+b02.pack()
+b02.place(x=320,y=250)
 
 ventana.mainloop()
